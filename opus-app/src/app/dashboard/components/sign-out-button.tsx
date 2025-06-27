5ad818a703251a4ed7150268b7bff9d7d45df77b
Rhuan Victor <212762553+OpusHub@users.noreply.github.com>
2025-06-27 18:56:31 -0300

    feat: enhance sign-out button with loading state and improved styling

--- opus-app/src/app/dashboard/components/sign-out-button.tsx
+++ opus-app/src/app/dashboard/components/sign-out-button.tsx
@@ -2,13 +2,18 @@
 
 import { Button } from "@/components/ui/button";
 import { authClient } from "@/lib/auth-client";
+import { Loader2 } from "lucide-react";
 import { useRouter } from "next/dist/client/components/navigation";
+import { useState } from "react";
 
 
 const SignOutButton = () => {
     const router = useRouter();
-  return <Button onClick={ () => { 
+    const [click, setClick] = useState(false);
 
+
+  return <Button className="min-w-[120px]" onClick={ () => {
+    setClick(true);
     authClient.signOut({
       fetchOptions: {
         onSuccess: () => {
@@ -18,7 +23,9 @@ const SignOutButton = () => {
     });
     
 
-  }}>Sair</Button>;
+  }}>{
+    click ? <Loader2 className="animate-spin" /> : "Sair"
+  }</Button>;
 };
 
 export default SignOutButton;
